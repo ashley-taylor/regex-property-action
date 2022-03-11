2cbb109e9b33362f0ba2140e4361bdec4200998b
Austen Stone <austenstone@github.com>
2022-03-11 16:29:53 -0500

    cleanup and minify

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const core = require('@actions/core');
 
-
 try {
-    const value = core.getInput('value');
-    const regex = core.getInput('regex');
-    const flags = core.getInput('flags');
-    const replacement = core.getInput('replacement');
-    
-    const re = new RegExp(regex, flags);
-    core.setOutput("value", value.replace(re, replacement));
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  const value = core.getInput('value');
+  const regex = core.getInput('regex');
+  const flags = core.getInput('flags');
+  const replacement = core.getInput('replacement');
+
+  const re = new RegExp(regex, flags);
+  core.setOutput("value", value.replace(re, replacement));
+} catch (error) {
+  core.setFailed(error.message);
+}
